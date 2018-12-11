468daa6155d63d9beb905857774cc7a0db3bb1bd
ShadowJoe <shwdowjoe@163.com>
2018-12-11 14:01:23 +0800

    修改和实现Fonour的.NetCore项目，迁入部分表现层代码。

--- FonourWeek/Startup.cs
+++ FonourWeek/Startup.cs
@@ -15,6 +15,7 @@ namespace FonourWeek
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,13 +23,29 @@ namespace FonourWeek
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
+                app.UseExceptionHandler("/Shared/Error");
+            }
+            else
+            {
+                app.UseExceptionHandler("/Shared/Error");
             }
 
-            app.Run(async (context) =>
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(route =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                route.MapRoute(
+                    name: "Default",
+                    template: "{controller=Login}/{action=Index}/{id?}"
+                    );
             });
+
+            app.UseStaticFiles();
+            //app.Run(async (context) =>
+            //{
+            //    var requestStr = context.Request.QueryString.Value;
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
