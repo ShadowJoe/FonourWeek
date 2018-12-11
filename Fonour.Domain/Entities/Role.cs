47f5a1045236f1558a156a6234ad659958139a17
ShadowJoe <zhou.joe@msn.cn>
2018-12-11 16:02:02 +0800

    添加项目管理文件夹

---
+++ Fonour.Domain/Entities/Role.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fonour.Domain.Entities
+{
+    class Role
+    {
+    }
+}
---
+++ Fonour.Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fonour.Domain
+{
+    public abstract class Entity<TPrimaryKey>
+    {
+        public virtual TPrimaryKey Id { get; set; }
+    }
+
+    public abstract class Entity: Entity<Guid>
+    {
+
+    }
+}
